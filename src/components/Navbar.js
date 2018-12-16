f3aa7efc254ecc0a04535c4cc3d46cd2d04bc961
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2018-12-17 03:52:41 +0530

    Material UI tab added instead navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = (props) => {
-    // setTimeout(() => {
-    //     props.history.push('/about')
-    // }, 2000)
-    return (
-        <nav className="nav-wrapper red darken-3">
+class Navbar extends React.Component {
+
+   render() {
+       return (
+        <div>
+            <Tabs>
+                <Tab label="Home" 
+                     containerElement={<Link to="/"/>} 
+                     style={styles}>
+                </Tab>
+                <Tab label="About" 
+                     containerElement={<NavLink to="/about"/>} 
+                     style={styles}>
+                </Tab>
+                <Tab label="Contact" 
+                     containerElement={<NavLink to="/contact"/>} 
+                     style={styles}>
+                </Tab>
+            </Tabs>
+
+        {/* <nav className="nav-wrapper red darken-3">
             <div className="container">
-              <a href=" " className="brand-logo">Poke'Times</a>
-              <ul className="right">
-                  <li><Link to="/">Home</Link></li>
-                  <li><NavLink to="/about">About</NavLink></li>
-                  <li><NavLink to="/contact">Contact</NavLink></li>
-              </ul>
+                <a href=" " className="brand-logo">Poke'Times</a>
+                <ul className="right">
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
             </div>
-        </nav>
-    );
+        </nav>  */}
+        </div>
+       );
+   }
+}
+
+let styles = {
+    backgroundColor: "#921414", 
+    padding: "10px", 
+    fontSize: "18px",
+    textTransform: "properCase"
 }
 
 export default withRouter(Navbar);
