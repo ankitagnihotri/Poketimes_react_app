346f4c2242e7c9976aa45520e8ee051119eb7ea3
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2018-12-17 14:24:57 +0530

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,25 +12,20 @@ module.exports = {
     module: {
         rules: [  // It is important to remember that when defining rules in your webpack config, you are defining them under module.rules and not rules. For your benefit, webpack will warn you if this is done incorrectly.
             {
-                test: /\.js?/,  // The test property identifies which file or files should be transformed.
-                loader: 'babel-loader',
+                test: /\.jsx$|\.es6$|\.js$/,  // The test property identifies which file or files should be transformed.
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react'],
+                    }
+                },
                 exclude: /node_module/,
-
-                options: {
-                    presets: [
-                      '@babel/preset-env',
-                      {
-                        plugins: [
-                          '@babel/plugin-proposal-class-properties'
-                        ]
-                      }
-                    ]
-                  },
             },
 
-            { 
-                test: /\.css$/, 
-                use: 'css-loader' },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
             {
                 test: /\.(jpe?g|png|gif|svg)$/i, 
                 loader: "file-loader?name=/public/icons/[name].[ext]"
